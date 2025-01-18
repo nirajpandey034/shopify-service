583058969e21da2046b40a25019c8047a84b6a0c
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2025-01-18 18:39:39 +0530

    Added Delete and Update services

--- src/main/java/com/nkp/shopify/controller/ProductController.java
+++ src/main/java/com/nkp/shopify/controller/ProductController.java
@@ -1,8 +1,9 @@
 package com.nkp.shopify.controller;
 
 import com.nkp.shopify.entities.ProductEntity;
-import com.nkp.shopify.repository.ProductRepository;
+import com.nkp.shopify.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,20 +13,40 @@ import java.util.List;
 public class ProductController {
 
     @Autowired
-    ProductRepository productRepository;
+    ProductService productService;
 
     @GetMapping
     public List<ProductEntity> showProducts() {
-        return productRepository.findAll();
+        return productService.getAllProducts();
     }
 
     @GetMapping("/{id}")
     public ProductEntity getProductById(@PathVariable long id) {
-        return productRepository.findById(id).orElse(null);
-    }
-    @PostMapping
-    public ProductEntity createProduct(@RequestBody ProductEntity product) {
-        return productRepository.save(product);
+        return productService.getProductById(id);
     }
 
+    @PostMapping
+    public ProductEntity createProduct(@RequestBody ProductEntity product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable long id) {
+        return productService.deleteProduct(id);
+    }
+
+    @PutMapping("/updateTitle")
+    public ProductEntity updateProductname(@RequestBody ProductEntity product) {
+        return productService.updateProductname(product);
+    }
+
+    @PutMapping("/updateDescription")
+    public ProductEntity updateProductdescription(@RequestBody ProductEntity product) {
+        return productService.updateProductdescription(product);
+    }
+
+    @PutMapping("/updatePrice")
+    public ProductEntity updateProductprice(@RequestBody ProductEntity product) {
+        return productService.updateProductprice(product);
+    }
 }
---
+++ src/main/java/com/nkp/shopify/service/ProductService.java
@@ -0,0 +1,92 @@
+package com.nkp.shopify.service;
+
+import com.nkp.shopify.entities.ProductEntity;
+import com.nkp.shopify.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<ProductEntity> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public ProductEntity getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public ProductEntity saveProduct(ProductEntity product) {
+        try {
+            return productRepository.save(product);
+        } catch (Exception e) {
+            return new ProductEntity();
+        }
+    }
+
+    public ResponseEntity<?> deleteProduct(@PathVariable long id) {
+        try {
+            productRepository.deleteById(id);
+            return ResponseEntity.ok().body("{\"message\": \"Product deleted successfully\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"Some Error Occured\"}");
+        }
+    }
+
+    public ProductEntity updateProductname(@RequestBody ProductEntity product) {
+        try {
+            Optional<ProductEntity> productEntity = productRepository.findById(product.getId());
+            if (productEntity.isPresent()) {
+                ProductEntity existingProduct = productEntity.get();
+                existingProduct.setName(product.getName());
+                return productRepository.save(existingProduct);
+            } else {
+                throw new RuntimeException("Product not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ProductEntity();
+        }
+    }
+
+    public ProductEntity updateProductdescription(@RequestBody ProductEntity product) {
+        try {
+            Optional<ProductEntity> productEntity = productRepository.findById(product.getId());
+            if (productEntity.isPresent()) {
+                ProductEntity existingProduct = productEntity.get();
+                existingProduct.setDescription(product.getDescription());
+                return productRepository.save(existingProduct);
+            } else {
+                throw new RuntimeException("Product not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ProductEntity();
+        }
+    }
+
+    public ProductEntity updateProductprice(@RequestBody ProductEntity product) {
+        try {
+            Optional<ProductEntity> productEntity = productRepository.findById(product.getId());
+            if (productEntity.isPresent()) {
+                ProductEntity existingProduct = productEntity.get();
+                existingProduct.setPrice(product.getPrice());
+                return productRepository.save(existingProduct);
+            } else {
+                throw new RuntimeException("Product not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ProductEntity();
+        }
+    }
+}
